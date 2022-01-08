9ef7a6e54ba75c55be3c63adf39a0f4f1e8de25a
Eslam Hamada <eh744272@gmail.com>
2022-01-08 11:39:55 -0800

    add file

--- app.js
+++ app.js
@@ -21,12 +21,3 @@ arrows.forEach((arrow, i) => {
 });
 
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-  setInterval(loader, 3000);
-}
-
-window.onload = fadeOut();s
