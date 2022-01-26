4d59eaebacea9e8e9cdd6b0cdb92978e22769730
Eslam Hamada <eh744272@gmail.com>
2022-01-26 05:01:52 +0200

    add new desgin

--- app.js
+++ app.js
@@ -20,4 +20,24 @@ arrows.forEach((arrow, i) => {
   console.log(Math.floor(window.innerWidth / 270));
 });
 
+let ham = document.querySelector('.ham')
+let menu = document.querySelector('.navbar')
 
+
+ham.addEventListener("click" , () => {
+    ham.classList.toggle("active")
+    menu.classList.toggle("active")
+
+})
+
+
+const myButton = document.getElementById('up')
+
+
+myButton.addEventListener('click', function() {
+  window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+  });
+});
